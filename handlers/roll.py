da6b026bfce45bdbedb0d6a6c593cc69a475a7a9
Rebecca C. Nelson <rebecca.c.nelson@target.com>
2018-01-23 21:41:39 -0600

    Implemented limits

--- handlers/roll.py
+++ handlers/roll.py
@@ -26,11 +26,22 @@ class Handler(MessageHandler):
                     sides = int(parts[1])
                 except (IndexError, ValueError):
                     client.send_message(message.channel, "Not in XdY format; assuming 1d6")
-            rolls = ""
-            total = 0
-            for x in range(count):
-                r = random.randint(1, sides)
-                rolls += str(r) + ", "
-                total += r
-
-            await client.send_message("Rolling {0:d}d{1:d}...\n{2:s}\nTotal: {3:d}".format(count, sides, rolls, total))
+            if sides > 100:
+                msg = "Sides > 100 not supported."
+            elif sides < 2:
+                msg = "There is no such thing as a one-sided die."
+            elif count > 100:
+                msg = "More than 100 dice per roll not supported. Are you running Shadowrun or something?"
+            elif count < 1:
+                msg = "Okay, I will roll " + str(count) + " dice. That is less than 1. You fail the roll. Rocks fall,"
+                msg += " everyone dies. The end."
+            else:
+                rolls = ""
+                total = 0
+                for x in range(count):
+                    r = random.randint(1, sides)
+                    rolls += str(r) + ", "
+                    total += r
+                msg = message.author.mention + " rolled {0:d}d{1:d}..."
+                msg += "\n{2:s}\nTotal: {3:d}".format(count, sides, rolls, total)
+            await client.send_message(msg)
