78b855f8e7dee75c62e727c1bfe873e5180d4c6c
topoftheyear <topoftheyear@Gmail.com>
2018-01-24 12:22:40 -0600

    Fixed 8ball
    Catches empty messages

--- handlers/8ball.py
+++ handlers/8ball.py
@@ -4,7 +4,8 @@ from handlers.message_handler import MessageHandler
 
 class Handler(MessageHandler):
     def __init__(self):
-        self.signal = "!test"
+        super().__init__()
+        self.signal = "!8ball"
 
         # displayed when !help is called
         self.description = self.signal + " : Life advice, several keystrokes away!"
@@ -19,35 +20,37 @@ class Handler(MessageHandler):
     async def handle_message(self, client, message):
 
         if message.content.startswith(self.signal):
-
-            responses = [
-            "It is certain",
-            "It is decidedly so",
-            "Without a doubt",
-            "Yes definitely",
-            "You may rely on it",
-            "As I see it, yes",
-            "Most likely",
-            "Outlook good",
-            "Yes",
-            "Signs point to yes",
-            "Reply hazy try again",
-            "Ask again later",
-            "Better not tell you now",
-            "Cannot predict now",
-            "Concentrate and ask again",
-            "Don't count on it",
-            "My reply is no",
-            "My sources say no",
-            "Outlook not so good",
-            "Very doubtful",
-            ]
-            
-            choice = 1
-            
-            if "Ajay" in message:
-                choice = random.randint(0,10)
+            if len(message.content.split(" ")) < 2:
+                await client.send_message(message.channel, "What?")
             else:
-                choice = random.randint(0,19)
+                responses = [
+                "It is certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Don't count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful",
+                ]
             
-            await client.edit_message(message.channel, responses[choice])
+                choice = 1
+            
+                if "ajay" in message.content.lower():
+                    choice = random.randint(0,10)
+                else:
+                    choice = random.randint(0,19)
+            
+                await client.send_message(message.channel, responses[choice])
