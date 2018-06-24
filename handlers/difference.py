d32b5d418ba9e704dd73643a876dc5d0c8b10a01
topoftheyear <topoftheyear@Gmail.com>
2018-06-23 19:02:07 -0500

    Improved difference function
    Changed time.sleep to await asyncio.sleep

--- handlers/difference.py
+++ handlers/difference.py
@@ -50,101 +50,101 @@ class DifferenceHandler(MessageHandler):
                     else:
                         await client.send_message(message.channel, "Well")
                     
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Um")
                     else:
                         await client.send_message(message.channel, "Uh")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "The first thing")
                     else:
                         await client.send_message(message.channel, "The first is")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Ah")
                     else:
                         await client.send_message(message.channel, "Hm")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                         
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Well its an object idea thing")
                     else:
                         await client.send_message(message.channel, "Its an idea or object, thing")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "That")
                     else:
                         await client.send_message(message.channel, "That uh")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Exists")
                     else:
                         await client.send_message(message.channel, "Does things")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     await client.send_message(message.channel, "Maybe")
                     
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Where as the second")
                     else:
                         await client.send_message(message.channel, "But the second")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Eh")
                     else:
                         await client.send_message(message.channel, "Er")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "May or may not do that too")
                     else:
                         await client.send_message(message.channel, "Doesn't, or, does")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Or something like that")
                     else:
                         await client.send_message(message.channel, "Or the complete opposite")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     await client.send_message(message.channel, "Maybe")
                         
-                    time.sleep(random.random() * 2)
+                    await asyncio.sleep(random.random() * 2)
                     
                     #Checks for a repeat
                     if (bool(random.getrandbits(1))):
                         await client.send_message(message.channel, "Wait")
-                        time.sleep(random.random() * 2)
+                        await asyncio.sleep(random.random() * 2)
                         if (bool(random.getrandbits(1))):
                             await client.send_message(message.channel, "Yeah I got that completely wrong, let me try again")
                         else:
                             good = True
                             await client.send_message(message.channel, "No")
-                            time.sleep(random.random() * 2)
+                            await asyncio.sleep(random.random() * 2)
                             await client.send_message(message.channel, "Yeah, that's right")
                     else:
                         await client.send_message(message.channel, "Yeah")
                         good = True
                 
-                time.sleep(random.random() * 2)
+                await asyncio.sleep(random.random() * 2)
                 await client.send_message(message.channel, "Hope that helped.")
                 
