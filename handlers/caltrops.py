2328e196993b912c16adc0f9d1078cb94eb494bb
Sean Hagen <sean.r.hagen@gmail.com>
2021-05-31 23:47:03 -0500

    Added more content

--- handlers/caltrops.py
+++ handlers/caltrops.py
@@ -17,7 +17,7 @@ class caltropHandler(MessageHandler):
         self.signal = "!caltrop"
 
         # params to dispay in help meesages
-        self.params = ""
+        self.params = "<type | either none, computers, problem or unusual>"
 
         # displayed when !help is called
         self.short_description = " Returns an interesting Wiki article to distract you. "
@@ -25,13 +25,33 @@ class caltropHandler(MessageHandler):
         # displatyed when !help caltrops is called
         self.long_description = " Inspired by XKCD 2467. Essentially just a scrape of https://en.wikipedia.org/wiki/Wikipedia:Unusual_articles "
 
-        with open("../content/caltrops.json") as fl:
-            self.caltrops = json.load(fl)
+        with open("../content/unusual.json") as fl:
+            self.unusual = json.load(fl)
+            
+        with open("../content/problem.json") as fl:
+            self.problem = json.load(fl)
+        
+        with open("../content/computers.json") as fl:
+            self.computers = json.load(fl)
 
 
     async def handle_message(self, client, message, state):
+        
         if message.content.lower().startswith(self.signal):
-            link, desc = random.choice(list(self.caltrops.items()))
+            split = message.content.split(" ", 1)
+            link, desc = ('','')
+            if not len(split) > 1:
+                choice = random.choice([self.unusual, self.problem, self.computers])
+                link, desc = random.choice(list(choice.items()))
+            else:
+                target = split[1]
+                if target == 'problem':
+                    link, desc = random.choice(list(self.problem.items()))
+                elif target == 'computer':
+                    link, desc = random.choice(list(self.computers.items()))
+                else:
+                    link, desc = random.choice(list(self.unusual.items()))
+                    
             try: 
                 send = "{0} | {1} ".format(desc, link)
             except KeyError:
